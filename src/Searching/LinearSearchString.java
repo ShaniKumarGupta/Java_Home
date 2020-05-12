44de3a6d94e5b0d619e89b9f8ed3160d0cb522ab
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-05-13 00:03:37 +0530

    Perform linear search operation on an Array of String

---
+++ src/Searching/LinearSearchString.java
@@ -0,0 +1,30 @@
+package Searching;/* shani gupta 12-05-2020 23:52 12*/
+
+import java.util.Scanner;
+
+public class LinearSearchString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of string to be stored in Array :");
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        int temp = 0;
+        System.out.println("Enter the string into Array :");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.next();
+        }
+
+        System.out.println("Enter the string to be search :");
+        String str = sc.next();
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].equals(str)){
+                System.out.println("String "+str+" found at index "+i);
+                temp+= 1;
+            }
+        }
+        if(temp == 0){
+            System.out.println("String not present in an Array");
+        }
+    }
+}
