686110a0cb2938460ef9bfd5f2358e0a7a376b8e
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-05-13 20:24:52 +0530

    Going to perform sorting operation

---
+++ src/Sorting/MergeSort.java
@@ -0,0 +1,4 @@
+package Sorting;/* shani gupta 13-05-2020 20:24 13*/
+
+public class MergeSort {
+}
---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,4 @@
+package Sorting;/* shani gupta 13-05-2020 20:23 13*/
+
+public class SelectionSort {
+}
