9655541d013955d86b41723762b699085cf1aebe
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-06-05 22:54:14 +0530

    create class named classExample

---
+++ src/Basic/classExample.java
@@ -0,0 +1,4 @@
+package Basic;/* shani gupta 05-06-2020 22:53 05*/
+
+public class classExample {
+}
--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -1,4 +1,10 @@
 package Sorting;/* shani gupta 13-05-2020 20:23 13*/
 
+import java.util.Scanner;
+
 public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
 }
