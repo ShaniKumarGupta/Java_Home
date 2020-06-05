bb233f52acf3cc7feed6303f17098271ba29bd3a
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-06-05 23:14:30 +0530

    declared static, instance and local variable

--- src/Basic/classExample.java
+++ src/Basic/classExample.java
@@ -1,4 +1,20 @@
 package Basic;/* shani gupta 05-06-2020 22:53 05*/
 
+// create class named classExample
 public class classExample {
-}
+    // There are three types of variable: local variable, instance variable and static variable
+    String breed; // instance variable
+    int age;
+    static int m = 50; // static variable
+    String color;
+
+    void barking(){
+    }
+
+    void hungry(){
+        String required; // local variable
+    }
+
+    void sleeping(){
+    }
+} // end of class
