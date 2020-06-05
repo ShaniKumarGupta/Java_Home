bb7b27a602cac0bb3bba9e8ce6ab8eebe11fbae1
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-06-05 23:24:41 +0530

    create object for class objectExample named as obj and obj1

--- src/Basic/objectExample.java
+++ src/Basic/objectExample.java
@@ -1,4 +1,14 @@
 package Basic;/* shani gupta 05-06-2020 23:17 05*/
 
 public class objectExample {
+    public objectExample(String name){
+        // This constructor has one parameter, name.
+        System.out.println("Passed name is "+ name);
+    }
+
+    public static void main(String[] args) {
+        // following statement would create an object obj and obj1
+        objectExample obj = new objectExample("Shani");
+        objectExample obj1 = new objectExample("Gupta");
+    }
 }
